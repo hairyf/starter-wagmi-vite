a0f18f004e24edb4bb991e41d31632083bbaf5ca
TuiMao233 <wwu710632@gmail.com>
2024-05-14 14:46:22 +0800

    feat: support ethers v6 and adapters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
 import { withLayout } from '../layout'
-import { getContract } from '@/utils'
+import { getEthersContract } from 'wagmi-ethers-adapters/ethers-v6'
 import { contracts } from '@/config'
 
 function Page() {
   const { address, isConnected } = useAccount()
   async function transfer() {
-    const erc20 = getContract(contracts.ERC20)
+    const erc20 = getEthersContract(contracts.ERC20)
     const transaction = await erc20.transfer(address!, 0)
-    await transaction.wait()
+    console.log(transaction)
   }
   return (
     <>
