c7f16f1af2c1f2c3b1d4802a8032ad168856e13a
TuiMao233 <wwu710632@gmail.com>
2024-05-14 15:01:19 +0800

    chore: update eslint config

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,55 +2,53 @@
 export {}
 declare global {
   interface ImportMetaEnv {
-    readonly 'VITE_APP_CHAIN_ID': number
+
   }
   namespace NodeJS {
     interface ProcessEnv {
-      readonly 'VITE_APP_CHAIN_ID': number
       readonly 'ALLUSERSPROFILE': string
+      readonly 'APPCODE_VM_OPTIONS': string
       readonly 'APPDATA': string
-      readonly 'ChocolateyInstall': string
-      readonly 'ChocolateyLastPathUpdate': number
+      readonly 'asl.log': string
       readonly 'CHROME_CRASHPAD_PIPE_NAME': string
+      readonly 'CLION_VM_OPTIONS': string
+      readonly 'COLOR': number
       readonly 'COLORTERM': string
       readonly 'CommonProgramW6432': string
       readonly 'COMPUTERNAME': string
       readonly 'ComSpec': string
+      readonly 'DataGrip': string
+      readonly 'DATAGRIP_VM_OPTIONS': string
+      readonly 'DATASPELL_VM_OPTIONS': string
+      readonly 'DEVECOSTUDIO_VM_OPTIONS': string
       readonly 'DriverData': string
+      readonly 'EDITOR': string
       readonly 'FPS_BROWSER_APP_PROFILE_STRING': string
       readonly 'FPS_BROWSER_USER_PROFILE_STRING': string
+      readonly 'GATEWAY_VM_OPTIONS': string
       readonly 'GIT_ASKPASS': string
+      readonly 'GOLAND_VM_OPTIONS': string
       readonly 'HOME': string
       readonly 'HOMEDRIVE': string
       readonly 'HOMEPATH': string
+      readonly 'IDEA_VM_OPTIONS': string
       readonly 'INIT_CWD': string
-      readonly 'IOJS_ORG_MIRROR': string
+      readonly 'JETBRAINSCLIENT_VM_OPTIONS': string
+      readonly 'JETBRAINS_CLIENT_VM_OPTIONS': string
       readonly 'LANG': string
       readonly 'LOCALAPPDATA': string
       readonly 'LOGONSERVER': string
       readonly 'NODE': string
-      readonly 'NODEJS_ORG_MIRROR': string
       readonly 'NODE_ENV': string
       readonly 'NODE_PATH': string
-      readonly 'NODIST_IOJS_MIRROR': string
-      readonly 'NODIST_NODE_MIRROR': string
       readonly 'NUMBER_OF_PROCESSORS': number
-      readonly 'NVMW_IOJS_ORG_MIRROR': string
-      readonly 'NVMW_NODEJS_ORG_MIRROR': string
-      readonly 'NVMW_NPM_MIRROR': string
-      readonly 'NVM_HOME': string
-      readonly 'NVM_IOJS_ORG_MIRROR': string
-      readonly 'NVM_NODEJS_ORG_MIRROR': string
-      readonly 'NVM_SYMLINK': string
-      readonly 'OculusBase': string
       readonly 'OneDrive': string
-      readonly 'OneDriveConsumer': string
       readonly 'ORIGINAL_XDG_CURRENT_DESKTOP': string
       readonly 'OS': string
       readonly 'Path': string
       readonly 'PATHEXT': string
+      readonly 'PHPSTORM_VM_OPTIONS': string
       readonly 'PNPM_HOME': string
-      readonly 'PNPM_SCRIPT_SRC_DIR': string
       readonly 'PROCESSOR_ARCHITECTURE': string
       readonly 'PROCESSOR_IDENTIFIER': string
       readonly 'PROCESSOR_LEVEL': number
@@ -60,7 +58,11 @@ declare global {
       readonly 'PROMPT': string
       readonly 'PSModulePath': string
       readonly 'PUBLIC': string
+      readonly 'PYCHARM_VM_OPTIONS': string
+      readonly 'RIDER_VM_OPTIONS': string
+      readonly 'RUBYMINE_VM_OPTIONS': string
       readonly 'SESSIONNAME': string
+      readonly 'STUDIO_VM_OPTIONS': string
       readonly 'SystemDrive': string
       readonly 'SystemRoot': string
       readonly 'TEMP': string
@@ -69,11 +71,12 @@ declare global {
       readonly 'TMP': string
       readonly 'USERDOMAIN': string
       readonly 'USERDOMAIN_ROAMINGPROFILE': string
-      readonly 'USERNAME': number
+      readonly 'USERNAME': string
       readonly 'USERPROFILE': string
-      readonly 'VBOX_HWVIRTEX_IGNORE_SVM_IN_USE': number
-      readonly 'VCINSTALLDIR': string
+      readonly 'WEBIDE_VM_OPTIONS': string
+      readonly 'WEBSTORM_VM_OPTIONS': string
       readonly 'windir': string
+      readonly 'WXDRIVE_START_ARGS': string
     }
   }
 }
